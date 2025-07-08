535be5560ab09896378164b70fc6d52133317b3f
Federicorocco1 <48479602+Federicorocco1@users.noreply.github.com>
2025-07-08 21:10:55 +0200

    1.13 done

--- Part1/Anecdotes/src/App.jsx
+++ Part1/Anecdotes/src/App.jsx
@@ -14,15 +14,34 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
 
+  const voted = Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0)
+  
+  const [vote, setVote] = useState([...voted])
+
+  console.log(voted[selected])
+
   const generateAnecdote = () => {setSelected(Math.floor(Math.random()*anecdotes.length))}
+
+  const incrementVotesArr = () => {
+    vote[selected]+=1
+    const votes = [...vote]
+    console.log('votes', votes)
+    console.log('selected',selected)
+    console.log('votes[selected]',votes[selected])
+    setVote(votes)
+  }
+
   console.log(anecdotes.length)
+
   return (
     <div>
       {anecdotes[selected]}
       <p></p>
+      <p>has {vote[selected]} votes</p>
+      <Button onClick={incrementVotesArr} text={'vote'} />
       <Button onClick={generateAnecdote} text={'next anecdote'} />
     </div>
   )
