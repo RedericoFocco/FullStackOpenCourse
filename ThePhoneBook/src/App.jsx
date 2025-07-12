76bb78494b975e9e028101238698578ecbc2d31b
Federicorocco1 <48479602+Federicorocco1@users.noreply.github.com>
2025-07-12 17:49:11 +0200

    setting state input form change

--- ThePhoneBook/src/App.jsx
+++ ThePhoneBook/src/App.jsx
@@ -1,69 +1,34 @@
-const Header = (course) => {
-  console.log(course)
-  return(
-  <>
-    <h1>Course {course.course_name}</h1>
-  </>
-  )
-}
-
-const Part = (part) => {
-  console.log('in part')
-  console.log(part)
-  return (
-    <p>Part {part.name} has {part.number} exercises</p>
-  )
-}
-
-const Content = ({content1,content2,content3}) => {
-  console.log(content1.name)
-  return (
-    <div>
-      <Part name={content1.name} number={content1.exercises} />
-      <Part name={content2.name} number={content2.exercises} />
-      <Part name={content3.name} number={content3.exercises} />
-    </div>
-  )
-}
-
-const Total = (tot) => {
-  console.log(tot)
-  return(
-  <>
-    <p>Total number of exercises {tot.tot_num_of_exercises}</p>
-  </>
-  )
-}
-
-const sum = (arr) => {
-  let init = 0
-  arr.forEach(x=>{init=init+x.exercises})
-  return init
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course =
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handlePersons = () => 
   {
-    name:'Half Stack application development',
-    parts :[{
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }]
+
+  }
+
+  const handleNewName = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value) //what user is actually typing
   }
 
   return (
     <div>
-      <Header course_name={course.name}/>
-      <Content content1={course.parts[0]} content2={course.parts[1]} content3={course.parts[2]}/>
-      <Total tot_num_of_exercises={sum(course.parts)}/>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
     </div>
   )
 }
