dbe38acd94d71feab356afc00f1afd8b236581df
Federicorocco1 <48479602+Federicorocco1@users.noreply.github.com>
2025-08-10 22:26:41 +0200

    3.3 done

--- ThePhoneBookBackend/index.js
+++ ThePhoneBookBackend/index.js
@@ -32,6 +32,22 @@ app.get('/api/personas',(request, response) => {
     response.json(personas)
 })
 
+app.get('/api/personas/:id',(request, response) => {
+    const id=request.params.id
+    console.log("id",id)
+    const [selectedInfo] = personas.filter(p=>p.id===id)
+    console.log(selectedInfo)
+    if(selectedInfo)
+    {
+        response.json(selectedInfo)
+    }
+    else
+    {
+        response.statusMessage=`No persona with id ${id} found`
+        response.status(404).end() //end important
+    }
+})
+
 
 app.get('/info',(request, response) => {
     response.send(`Phonebook has info for ${personas.length} people.<br> ${new Date().toString()}`)
