219a449589bdb18f5c7460e78c39bb7350f110a0
Federicorocco1 <48479602+Federicorocco1@users.noreply.github.com>
2025-08-21 20:49:01 +0200

    4.1 technically done

---
+++ BlogList/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config() // need to be imported before calling modules, models ecc
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const logger = require('./utils/logger')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGO_URI_BLOGS
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  logger.info('Entered [POST]')
+  const blog = new Blog(request.body)
+  logger.info('[POST] blog:',blog)
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
