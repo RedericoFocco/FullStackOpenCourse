65986e168feeea89ce2511c5de419797681a28bb
Federicorocco1 <48479602+Federicorocco1@users.noreply.github.com>
2025-08-24 16:20:06 +0200

    3.4 done - 20 min

--- BlogList/tests/dummy.test.js
+++ BlogList/tests/dummy.test.js
@@ -1,10 +1,60 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
-const listHelper = require('../utils/list_helper')
+const totalLikes = require('../utils/list_helper').totaLikes
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('total likes', () => {
+  
+const listNoBlog=[]  
+    
+const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+  const blogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('when list has 0 blog', () => {
+    const result = totalLikes(listNoBlog)
+    assert.strictEqual(result, 0)
+  })
+
+   test('when list has N blog', () => {
+    const result = totalLikes(blogs)
+    assert.strictEqual(result, 24)
+  })
 })
--- BlogList/utils/list_helper.js
+++ BlogList/utils/list_helper.js
@@ -1,7 +1,21 @@
+const { totalmem } = require("os")
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totaLikes = (blogsLikes) => 
+{
+    const reducer = (sum, item) => {
+    console.log('[list helper] item',item)
+    return sum + item.likes
+  }
+
+  return blogsLikes.length === 0
+    ? 0
+    : blogsLikes.reduce(reducer, 0) 
+}
+
 module.exports = {
-  dummy
+  totaLikes
 }
