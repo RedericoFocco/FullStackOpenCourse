b192c762f79e1ccb166d10b4ef08c6225064e674
Federicorocco1 <48479602+Federicorocco1@users.noreply.github.com>
2025-09-22 20:58:07 +0200

    approaching 4.8

--- BlogList/utils/config.js
+++ BlogList/utils/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config() // need to be imported before calling modules, models ecc
 
 const PORT = process.env.PORT
-const MONGO_DB_URI = process.env.MONGO_URI_BLOGS
+const MONGO_DB_URI = process.env.NODE_ENV==="test" ? 
+process.env.MONGO_URI_BLOGS_TEST :
+process.env.MONGO_URI_BLOGS
 
 module.exports = {PORT,MONGO_DB_URI}
