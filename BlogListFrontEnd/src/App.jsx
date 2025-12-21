dc7fbd56e36002568cd84008f92fc11cf7b47a22
Federicorocco1 <48479602+Federicorocco1@users.noreply.github.com>
2025-12-21 19:00:33 +0100

    continuning 5.2

--- BlogListFrontEnd/src/App.jsx
+++ BlogListFrontEnd/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
+  const [loginMsg, setLoginMsg] = useState('')
+  const [wrongLoginMsg, setWrongLoginMsg] = useState('') 
+  const [user, setUser] = useState(null) 
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,19 +17,17 @@ const App = () => {
     )  
   }, [])
 
+  const logoutForm = () => (
+    <>
+      <form onSubmit={handleLogout}>
+      <button type="submit">logout</button>
+      </form>
+      </>
+  ) 
 
-  const handleLogin = (event) => {
-    event.preventDefault()
-    console.log('logging in with', username, password)
-  }
-
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-
+  const loginForm = () => {
+      return (
+    <>
       <h2>Login</h2>
       <form onSubmit={handleLogin}>
         <div>
@@ -50,6 +52,60 @@ const App = () => {
         </div>
         <button type="submit">login</button>
       </form>
+      <p>{wrongLoginMsg}</p>
+      </>
+      )
+  }
+
+  const blogList = () => (
+    <>
+      <h2>blogs</h2>
+      <p>{loginMsg}</p>
+      <p>Your blogs:</p>
+      {blogs.filter((blog)=>blog.user_id.username === user.username).map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </>
+  ) 
+  
+  const handleLogout = async eventClick => {
+    eventClick.preventDefault()
+    setUser(null)
+    setWrongLoginMsg('')
+  }
+
+  const handleLogin = async eventClick => {
+    eventClick.preventDefault()
+    //console.log('logging in with', username, password)
+    try
+    {
+      const user = await loginService.loginPost({username,password})
+      //console.log("logging output:",loggingOutput)
+      if(user)
+      {
+        setLoginMsg(`${user.username} logged in`)
+        setTimeout(()=>{setLoginMsg(null)},2000)
+        console.log('logged!',user)
+        setUser(user)
+      }
+      else
+      {
+        setWrongLoginMsg('Error logging in')
+        console.log('mì')
+      }
+    }
+    catch
+    {
+      setWrongLoginMsg('Error logging in! Please check username and pwd')
+      console.log('catching login')
+    }
+  }
+
+  return (
+    <div>
+      {user && blogList()}
+      {user && logoutForm()}
+      {!user && loginForm()}  
     </div>
   )
 }
