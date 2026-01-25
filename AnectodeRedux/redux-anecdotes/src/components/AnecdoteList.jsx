692f85da0dccb6f65ed284b86650dc932a815418
Federicorocco1 <48479602+Federicorocco1@users.noreply.github.com>
2026-01-25 12:49:41 +0100

    6.9 done - "non banale", 3 hours

--- AnectodeRedux/redux-anecdotes/src/components/AnecdoteList.jsx
+++ AnectodeRedux/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,11 @@ import { useSelector,useDispatch } from 'react-redux'
 import { addNewVote,addNewAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state=>state)
+    const anecdotes = useSelector(({anecdotes,filter})=>{
+      console.log(`ANECDOTES:${anecdotes},FILTER:${filter}`)
+      return anecdotes.filter(a=>a.content.includes(filter))
+    }
+    )
     const dispatch = useDispatch()
     
     const vote = id => {
--- AnectodeRedux/redux-anecdotes/src/reducers/filterReducer.js
+++ AnectodeRedux/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,18 @@
 export const filterAnecdote = filterString => {
+  console.log('filterString',filterString)
   return  {
     type : 'FILTER',
-    payload : { filterString }
+    payload :  filterString
   }
 }
 
-const filterReducer = (state = fullAnecdotes, action) => {
+const filterReducer = (state='', action) => {
   console.log('state now: ', state)
   console.log('action', action)
   switch(action.type)
   {
     case 'FILTER':
-      return state.filter(s=>s.includes(action.payload))
+      return action.payload
     default :
       return state
   }
